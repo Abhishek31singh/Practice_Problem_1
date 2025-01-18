94f0b8ee5a2b18081d731425e01b97c5b2bcfe52
Abhishek31singh <arya31thakur@gmail.com>
2025-01-18 15:01:08 +0530

    First commit

---
+++ GLA_Capgemini/PracticeProblem.java
@@ -0,0 +1,17 @@
+public class PracticeProblem{
+public static void main(String args[]){
+System.out.println("Even number form 1 to 10");
+for(int i=1;i<=10;i++){
+if(i%2==0){
+System.out.print(i+" ");
+}
+}
+System.out.println("odd");
+for(int i=1;i<=10;i++){
+if(i%2!=0){
+System.out.print(i+" ");
+}
+
+}
+}
+}
